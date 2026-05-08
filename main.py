3ae20d8448960304b3e7d7aa0465f8ce83cb3045
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-05-08 16:19:33 -0400

    adding character

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 w = 30
 l = 10
+
+snake = [(15, 5)]
+
+def printChar(char):
+    print(char)
 def printBoard():
+    
     for i in range(w):
         print("-", end="")
     print()
     for i in range(l):
         print("|" + " "*(w-2)+"|")
+        if i==5:
+            printChar("\tOOOX")
     for i in range(w):
         print("-", end="")
-    
+
+
 printBoard()
+
 
