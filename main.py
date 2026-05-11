82b30739890d2a4346ce474c7494d26f81948ef9
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-05-10 23:37:27 -0400

    update

--- main.py
+++ main.py
@@ -3,21 +3,14 @@ l = 10
 
 snake = [(15, 5)]
 
-def printChar(char):
-    print(char)
+
 def printBoard():
     
-    for i in range(w):
-        print("-", end="")
-    print()
-    for i in range(l):
-        print("|" + " "*(w-2)+"|")
-        if i==5:
-            printChar("\tOOOX")
-    for i in range(w):
-        print("-", end="")
-
-
+    for row in range(l):
+        line = ""
+        for col in range(w):
+            if (col, row) in snake:
+                line += "O"
 printBoard()
 
 
